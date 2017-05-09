dd20dbf5fdb16742ead5cd7bc6102586af474598
Jordan Lenaburg <jordan.lenaburg@gmail.com>
2017-05-09 11:49:08 -0600

    Updated files

--- projects/penguins/main.js
+++ projects/penguins/main.js
@@ -3,24 +3,33 @@ function Party(name, population) {
     population = 1000000;
 }
 
-
-
 var penguins = new Party("Penguins");
 var commie = new Party("Communists");
 
 function sendNuke(party, onHit, onMiss) {
+    if (party === commie) {
+        var others = penguins;
+    } else {
+        var others = commie;
+    }
+    if (party.population < 1) {
+        console.log(others.name + " has defeated " + party.name + "! It is a sad day for the world.");
+        return;
+    }
     var rand = Math.random();
-    party = ; //object who will be attacked
+    //    party; //object who will be attacked
     if (rand < .50) {
         function onHit(party) {
-            console.log(party.name + " has hit the enemy with a Nuculear Strike!");
-            party.population -= Math.floor(rand * 10000)
-            console
+            console.log(others.name + " has hit the enemy with a Nuculear Strike!");
+            party.population -= Math.floor(rand * 10000);
+            console.log(party.name + " now has " + party.population + " people left!");
+            nextAttack();
             //callback function to be called if attack is successful
         }
     } else {
         function onMiss(party) {
-            console.log(party.name + " has missed the enemy with an unsuccessful Nuculear Strike!");
+            console.log(others.name + " has missed " + party.name + " with an unsuccessful Nuculear Strike!");
+            nextAttack();
         } //callback function to be called if attack is unsuccessful
     }
 }
@@ -28,15 +37,15 @@ function sendNuke(party, onHit, onMiss) {
 
 var start = Math.random();
 if (start < .50) {
-    //penguin attack
+    sendNuke(commie);
 } else {
-    //commie attack
+    sendNuke(penguins);
 }
 
 function nextAttack() {
     if (party === penguins) {
-        sendNuke(commie)
+        sendNuke(commie);
     } else {
-        sendNuke(penguins)
+        sendNuke(penguins);
     }
 }
